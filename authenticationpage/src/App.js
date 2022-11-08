f0233b1165d263e1776270b932c341a481f42ae6
santhosh <santhoshkaliannann@gmail.com>
2022-11-08 19:55:57 +0530

    Access Token Check Valid  in App File

--- authenticationpage/src/App.js
+++ authenticationpage/src/App.js
@@ -4,8 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Profile from './Pages/Profile';
 import PrivateRoute from './Components/PrivateRoute';
-import { getToken ,setUserSession,removeUserSession} from './Components/Common';
-import axios from 'axios';
+import { getToken} from './Components/Common';
 
 
 function App() {
@@ -13,18 +12,10 @@ function App() {
 
   useEffect(() => {
     const token = getToken();
-    if (!token) {
-      return;
-    }
-
-    axios.get(`https://28eb-103-141-51-42.in.ngrok.io`).then(response => {
-      setUserSession(response.data.token, response.data.user);
+    if (token) {
       setAuthLoading(true);
-    }).catch(error => {
-      removeUserSession();
-      setAuthLoading(false);
-    });
-  }, []);
+    }
+  },[])
   return (
     <div className="App">
       <BrowserRouter>
