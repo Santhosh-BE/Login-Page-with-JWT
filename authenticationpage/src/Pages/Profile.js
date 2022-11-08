0c80f3f311f3169b6604227229a0c8a52384ef57
santhosh <santhoshkaliannann@gmail.com>
2022-11-08 11:56:36 +0530

    Session Expired Function Added

--- authenticationpage/src/Pages/Profile.js
+++ authenticationpage/src/Pages/Profile.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { removeUserSession } from '../Components/Common';
+import { SessionExpired } from '../Components/SessionExpired';
+
 
 
 function Profile() {
-    const navigate = useNavigate();
-    const logoutbtn=()=>{
-       
-            navigate('/login')
-        }
+  const navigate = useNavigate();
+  const logoutbtn = () => {
+    removeUserSession()
+    navigate('/')
+  }
   return (
     <div>
-        <h1>Profile page</h1>
-    <button className='btn btn-danger' onClick={logoutbtn}>Logout</button>
+      <h1>Profile page</h1>
+      <button className='btn btn-danger' onClick={logoutbtn}>Logout</button>
+      <SessionExpired />
     </div>
   )
 }
